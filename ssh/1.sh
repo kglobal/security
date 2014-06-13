5e16d7501bd4657e6f339f59004170399d57fe51
Kenny Ha <kennyha@pkmobilepay.local>
2014-06-13 00:25:01 -0700

    initial

---
+++ ssh/1.sh
@@ -0,0 +1,32 @@
+clear
+cat copyright
+email="khaprograms@gmail.com"
+
+
+echo "#"
+echo "#|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+echo "# Description:"
+echo "#     A utility to create an ssh user to be used in the remote cloud."
+echo "#|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+echo "#"
+
+m1="# 1. sign into your remote linux server with sudo privileges"
+m2="# 2. create this script: create_ssh_user.sh"
+m3="# 3. chmod 777 create_ssh_user.sh"
+m4="# 4. update connect_to_remote_linux_server.sh with username and private key created from #2"
+m5="# 5. ./connect_to_remote_linux_server.sh"
+m6="# 6. Need further clarification?, email me: $email "
+
+echo "#"
+echo "$m1"
+echo "#"
+echo "$m2"
+echo "#"
+echo "$m3"
+echo "#"
+echo "$m4"
+echo "#"
+echo "$m5"
+echo "#"
+echo "$m6"
+echo "#"
---
+++ ssh/README.md
@@ -0,0 +1,5 @@
+#
+# From your command line, run these two commands to start.
+#
+chmod 777 1.sh ; ./1.sh
+
